5b045f816e657866f482f72ece0d54e5dfba19f5
Frank <62514408+frankbits@users.noreply.github.com>
2025-02-15 16:33:37 +0100

    initialize Robot outside of `loop()`

--- src/main.cpp
+++ src/main.cpp
@@ -98,23 +98,17 @@ class ArduinoMock: public ArduinoPinInterface{
 };
 
 Arduino arduino;
+Robot robot;
 
-
-void setup() {
-  // erzeugung der Objekte
-}
+void setup() {}
 
 void loop() {
-
   // 1. RoboterController fragt die Daten von SensorData ab z.B.: getCurrentSensorData()
   //    1.1. SensorData holt sich den aktuellen Zustand des Arduinos über das Pininterface
   //    1.2. SensorData muss die Daten verarbeiten (Umrechnungen der Drehzahl, ...)
   // 2. RoboterController entscheidet, welches Movement nötig ist und errechnet das Korrekturdelta
   // 3. RoboterController übergibt das Korrketurdelta an die Movement und führt die entsprechende bewegung aus
   // 4. Movement steuert über das Pininteface die Motoren an
-
-
-  Robot robot;
   robot.next();
 }
 
