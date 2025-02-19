f619c51eed592d246f003ed6671170346b088ffa
Frank <62514408+frankbits@users.noreply.github.com>
2025-02-19 10:45:58 +0100

    added Robot-DocBlocks

--- lib/Robot/include/Robot.h
+++ lib/Robot/include/Robot.h
@@ -4,12 +4,18 @@
 
 class Robot {
     private:
-      /* Schwellwerte für das Erfassen eines Hindernisses*/
+      /** Schwellwert der Distanz nach vorne, über den der Weg geradeaus frei ist */
       static const int THRESHOLD_FREE_F;
+      /** Schwellwert der Distanz nach links, über den der Weg links frei ist */
       static const int THRESHOLD_FREE_L;
+      /** Schwellwert der Distanz nach rechts, über den der Weg rechts frei ist */
       static const int THRESHOLD_FREE_R;
-  
-      
+
+      /** Objekt zur Ansteuerung beider Motoren mit verschiedenen definierten Manövern */
+      Movement movement;
+
+      /** Objekt zur Aktualisierung und Abfrage der Sensorwerte */
+      SensorData sensorData;
 
       /* Integrator der Drehzahlabweichung */
       float distanceDeltaIntegrator = 0;
@@ -25,13 +31,27 @@ class Robot {
       /* Prüft, ob der Weg rechts frei ist für den Roboter */
       bool rightIsFree();
 
+      /**
+       * Bewegt den Roboter vorwärts durch das Labyrinth.
+       * 
+       * Die Differenz zwischen dem Abstand zur linken Wand und dem Abstand zur rechten Wand wird verwendet, um den Roboter mittig zu halten.
+       * 
+       * @param distanceDelta die Differenz zwischen dem Abstand zur linken Wand und dem Abstand zur rechten Wand
+       * @param ms die Zeit, die der Roboter sich bewegen soll
+       */
       void moveForward(float distanceDelta, unsigned long ms = 100);
 
+      /**
+       * Visualisiert den gegebenen Abstand nach links und rechts in der Konsole,
+       * indem 'L' und 'R' jeweils so oft aufgeführt werden, wie der
+       * Abstand in cm beträgt.
+       * 
+       * @param distanceL der Abstand in cm von links
+       * @param distanceR der Abstand in cm von rechts
+       */
       void visualizeLeftAndRightDistance(float distanceL, float distanceR);
   
     public:
-      Movement movement;
       /* Berechnet den nächsten Schritt des Roboters beruhend auf den Messdaten der Sensoren */
       void next();
-      SensorData sensorData;
   };
