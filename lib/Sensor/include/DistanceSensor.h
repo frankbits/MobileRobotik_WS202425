320a881bf34916e7ddb971636fc74b219f3db574
Frank <62514408+frankbits@users.noreply.github.com>
2025-02-19 10:51:15 +0100

    added Distanceensor-DocBlocks

--- lib/Sensor/include/DistanceSensor.h
+++ lib/Sensor/include/DistanceSensor.h
@@ -1,11 +1,25 @@
 #include <NewPing.h> // Abstand berechnen
 
+/**
+ * Klasse zum Auslesen der Distanz über jeweils einen Ultraschallsensor.
+ */
 class DistanceSensor {
   private:
+    /** Maximal zu messende Distanz in cm */
     static const int MAX_DISTANCE;
+
+    /** Objekt zum Auslesen der Distanz über einen Ultraschallsensor */
     NewPing sensor;
 
   public:
+    /** 
+     * Initialisiert einen Ultraschallsensor mit gegebenem Trigger- und Echo-Pin
+     * 
+     * @param trigger Trigger-Pin des Ultraschallsensors
+     * @param echo Echo-Pin des Ultraschallsensors
+     */
     DistanceSensor(int trigger, int echo);
+
+    /** Gibt die aktuelle Distanz zurueck */
     float getCurrentDistance();
 };
